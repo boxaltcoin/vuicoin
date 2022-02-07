c969a4ec95904ebff319fb73f5311e0c7ceaaeb7
Box AltCoin <boxaltcoin@gmail.com>
2022-02-08 01:10:10 +0700

    Change Vuicoin

---
+++ contrib/devtools/gen-manpages.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+TOPDIR=${TOPDIR:-$(git rev-parse --show-toplevel)}
+SRCDIR=${SRCDIR:-$TOPDIR/src}
+MANDIR=${MANDIR:-$TOPDIR/doc/man}
+
+LITECOIND=${LITECOIND:-$SRCDIR/vuicoind}
+LITECOINCLI=${LITECOINCLI:-$SRCDIR/vuicoin-cli}
+LITECOINTX=${LITECOINTX:-$SRCDIR/vuicoin-tx}
+LITECOINQT=${LITECOINQT:-$SRCDIR/qt/vuicoin-qt}
+
+[ ! -x $LITECOIND ] && echo "$LITECOIND not found or not executable." && exit 1
+
+# The autodetected version git tag can screw up manpage output a little bit
+VUIVER=($($LITECOINCLI --version | head -n1 | awk -F'[ -]' '{ print $6, $7 }'))
+
+# Create a footer file with copyright content.
+# This gets autodetected fine for bitcoind if --version-string is not set,
+# but has different outcomes for bitcoin-qt and bitcoin-cli.
+echo "[COPYRIGHT]" > footer.h2m
+$LITECOIND --version | sed -n '1!p' >> footer.h2m
+
+for cmd in $LITECOIND $LITECOINCLI $LITECOINTX $LITECOINQT; do
+  cmdname="${cmd##*/}"
+  help2man -N --version-string=${VUIVER[0]} --include=footer.h2m -o ${MANDIR}/${cmdname}.1 ${cmd}
+  sed -i "s/\\\-${VUIVER[1]}//g" ${MANDIR}/${cmdname}.1
+done
+
+rm -f footer.h2m
