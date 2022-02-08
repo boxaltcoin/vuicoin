a4b4bbb83b745836d9069bb4ac62167bbbd56b96
Box AltCoin <boxaltcoin@gmail.com>
2022-02-08 21:41:35 +0700

    Change Unit

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -41,8 +41,8 @@ QString BitcoinUnits::longName(int unit)
     switch(unit)
     {
     case BTC: return QString("VUI");
-    case mBTC: return QString("lites");
-    case uBTC: return QString("photons");
+    case mBTC: return QString("namvui");
+    case uBTC: return QString("thangvui");
     default: return QString("???");
     }
 }
